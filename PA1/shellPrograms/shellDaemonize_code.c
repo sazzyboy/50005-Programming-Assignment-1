f9a62a4d9c96f761f35b9f21bf16e0e01e8af9e5
sazzyboy <43925277+sazzyboy@users.noreply.github.com>
2021-03-14 18:02:12 +0800

    to do 7

--- PA1/shellPrograms/shellDaemonize_code.c
+++ PA1/shellPrograms/shellDaemonize_code.c
@@ -13,7 +13,7 @@
 #include "shellPrograms.h"
 
 //TODO: change to appropriate path
-char *path = "/Users/natalie_agus/Dropbox/50.005 Computer System Engineering/2020/PA1 Makeshell Daemon/PA1/logfile_test.txt";
+char *path = "/Users/amrishsandhu/Documents/GitHub/50005-Programming-Assignment-1/PA1/logfile_test.txt";
 
 /*This function summons a daemon process out of the current process*/
 static int create_daemon()
@@ -23,13 +23,49 @@ static int create_daemon()
     // Incantation on creating a daemon with fork() twice
 
     // 1. Fork() from the parent process
-    // 2. Close parent with exit(1)
+    pid_t parentpid1= fork();
+    if (parentpid1>0){
+        exit(1);
+    }    // 2. Close parent with exit(1)
+    else if (parentpid1<0){
+        perror("CSESHELL ERROR YO");
+        return 1;
+    }
+    if (setsid()==-1){  
     // 3. On child process (this is intermediate process), call setsid() so that the child becomes session leader to lose the controlling TTY
-    // 4. Ignore SIGCHLD, SIGHUP
+        perror("CSESHELL ERROR YO");
+        return 1;
+    }
+    signal(SIGCHLD,SIG_IGN);    // 4. Ignore SIGCHLD, SIGHUP
+    signal(SIGHUP,SIG_IGN);    // 4. Ignore SIGCHLD, SIGHUP
+
+
+
     // 5. Fork() again, parent (the intermediate) process terminates
-    // 6. Child process (the daemon) set new file permissions using umask(0). Daemon's PPID at this point is 1 (the init)
-    // 7. Change working directory to root
-    // 8. Close all open file descriptors using sysconf(_SC_OPEN_MAX) and redirect fd 0,1,2 to /dev/null
+    pid_t  parentpid2 = fork();
+        if (parentpid2>0){
+        exit(1);
+    }    
+    else if (parentpid2<0){
+        perror("CSESHELL ERROR YO");
+        return 1;
+    }
+    if (setsid()==-1){  
+        perror("CSESHELL ERROR YO");
+        return 1;
+    }
+    umask(0);     // 6. Child process (the daemon) set new file permissions using umask(0). Daemon's PPID at this point is 1 (the init)
+
+    chdir("/");     // 7. Change working directory to root
+
+
+    for (int i = sysconf(_SC_OPEN_MAX); i>=0; i--){
+        close(i);
+    }
+    int fd0 =open("/dev/null",O_RDWR);    // 8. Close all open file descriptors using sysconf(_SC_OPEN_MAX) and redirect fd 0,1,2 to /dev/null
+
+    int fd1 =dup(0);
+    int fd2 =dup(0);
     // 9. Return to main
 
     return 1;
