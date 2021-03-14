154a4ab1a8e54243650d5fd2937573bcaf190efd
sazzyboy <43925277+sazzyboy@users.noreply.github.com>
2021-03-14 18:12:02 +0800

    to do 8 added

--- PA1/shellPrograms/shellCheckDaemon_code.c
+++ PA1/shellPrograms/shellCheckDaemon_code.c
@@ -10,15 +10,31 @@ int shellCheckDaemon_code()
    sprintf(command, "ps -efj | grep summond  | grep -v tty > output.txt");
 
    // TODO: Execute the command using system(command) and check its return value
-
+   if (system(command)==-1){
+      perror("CSESHELL ERROR YO");
+      return 1;
+   }
    free(command);
+   size_t size =32;
+   char *buffer = malloc(sizeof(char)*size);
+   if (buffer==NULL){
+      perror("CSESHELL ERROR YO");
+      return 1;
+   }
 
    int live_daemons = 0;
    // TODO: Analyse the file output.txt, wherever you set it to be. You can reuse your code for countline program
    // 1. Open the file
+   FILE *filepointer=fopen("output.txt","r");
    // 2. Fetch line by line using getline()
-   // 3. Increase the daemon count whenever we encounter a line
-   // 4. Close the file
+   if (filepointer!=NULL){
+      while (getline(&buffer,&size,filepointer) != -1){
+         live_daemons++;    // 3. Increase the daemon count whenever we encounter a line
+
+      }
+      fclose(filepointer);   // 4. Close the file
+   }
+
    // 5. print your result
 
    if (live_daemons == 0)
@@ -27,10 +43,8 @@ int shellCheckDaemon_code()
    {
       printf("There are in total of %d live daemons \n", live_daemons);
    }
-
-
    // TODO: close any file pointers and free any statically allocated memory 
-
+   free(buffer);
    return 1;
 }
 
